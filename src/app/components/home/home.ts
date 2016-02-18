c7d8d2d588a081d2b94832007cccbc4a5cd18a36
Valerii Vasin <valerii.vasin@gmail.com>
2016-02-18 09:00:14 +0100

    Refactor.

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,35 +1,14 @@
 import { Component } from 'angular2/core';
-import { Store } from '../../store';
-import { TodoList } from '../todos/todos';
+import { TodoListContainer } from '../todo-list/todo-list.container';
 
 @Component({
   selector: 'home',
   template: `
     <h3>Home Component</h3>
-    <todo-list [todos]="todos"></todo-list>
+    <todo-list-container></todo-list-container>
     <button (click)="onAddTodo()">Add todo</button>
   `,
-  directives: [TodoList]
+  directives: [TodoListContainer]
 })
 export class Home {
-  public todos: Object[];
-  private unsubscribe: Function;
-
-  constructor(private store: Store) {}
-
-  onAddTodo() {
-    this.store.dispatch({ type: 'CREATE_TODO' });
-  }
-
-  ngOnInit() {
-    this.todos = this.store.getState().todos;
-
-    this.unsubscribe = this.store.subscribe(() => {
-      this.todos = this.store.getState().todos;
-    });
-  }
-
-  ngOnDestroy() {
-    this.unsubscribe();
-  }
 }
---
+++ src/app/components/todo-list/todo-list.container.ts
@@ -0,0 +1,45 @@
+import { Component } from 'angular2/core';
+import { Store } from '../../store';
+import { TodoList } from '../todo-list/todo-list';
+
+const assign = (target, ...sources) => {
+  for (let source of sources) {
+    for (let prop of Object.keys(source)) {
+      target[prop] = source[prop];
+    }
+  }
+
+  return target;
+}
+
+@Component({
+  selector: 'todo-list-container',
+  template: `
+    <todo-list [todos]="todos"></todo-list>
+  `,
+  directives: [TodoList]
+})
+export class TodoListContainer {
+  public todos: Object[];
+  private unsubscribe: Function;
+
+  constructor(private store: Store) {}
+
+  mapStateToProps(state) {
+    return {
+      todos: state.todos
+    };
+  }
+
+  ngOnInit() {
+    assign(this, this.mapStateToProps(this.store.getState()));
+
+    this.unsubscribe = this.store.subscribe(() => {
+      this.todos = this.store.getState().todos;
+    });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe();
+  }
+}
