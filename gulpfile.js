a15dca32147f8c8432a02378190ec81158141155
Valerii Vasin <valerii.vasin@gmail.com>
2016-02-25 02:47:17 +0100

    Prepare for the build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,27 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const preprocess = require('gulp-preprocess');
+
 require('./tasks/webpack');
+
+const DESTINATION_FOLDER = 'dist/';
+
+gulp.task('html', () => {
+  gulp.src('src/index.html')
+    .pipe(preprocess())
+    .pipe(gulp.dest(DESTINATION_FOLDER));
+});
+
+gulp.task('favicon', () => {
+  gulp.src('src/favicon.ico')
+    .pipe(gulp.dest(DESTINATION_FOLDER));
+});
+
+gulp.task('css', () => {
+  gulp.src('src/css/*.css')
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(DESTINATION_FOLDER));
+});
+
+gulp.task('build', ['html', 'favicon', 'css', 'webpack:build']);
+gulp.task('build:dev', ['html', 'favicon', 'css', 'webpack:build:dev']);
