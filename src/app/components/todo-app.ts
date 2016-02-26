a9095c6205d207146108633e11e8c22942a55670
Valerii Vasin <valerii.vasin@gmail.com>
2016-02-26 11:19:09 +0100

    Remove div wrapper.

--- src/app/components/todo-app.ts
+++ src/app/components/todo-app.ts
@@ -10,21 +10,19 @@ import { ToggleAllContainer } from '../containers/toggle-all';
   selector: 'home',
   template: `
     <section id="todoapp">
-      <div>
-        <header class="header">
-          <h1>todos</h1>
-          <add-todo-container></add-todo-container>
-        </header>
-        <section id="main">
-          <toggle-all></toggle-all>
-          <todo-list-container></todo-list-container>
-        </section>
-        <footer id="footer">
-          <todos-counter-container></todos-counter-container>
-          <todos-filters></todos-filters>
-          <clear-completed></clear-completed>
-        </footer>
-      </div>
+      <header class="header">
+        <h1>todos</h1>
+        <add-todo-container></add-todo-container>
+      </header>
+      <section id="main">
+        <toggle-all></toggle-all>
+        <todo-list-container></todo-list-container>
+      </section>
+      <footer id="footer">
+        <todos-counter-container></todos-counter-container>
+        <todos-filters></todos-filters>
+        <clear-completed></clear-completed>
+      </footer>
     </section>
   `,
   directives: [
